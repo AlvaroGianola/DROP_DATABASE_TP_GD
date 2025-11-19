5ae7e7dfa216e857c8e8669ed18f5a47fc2e4bb9
Álvaro Federico Gianola Otamendi <alvarogianolao@gmail.com>
2025-11-18 22:05:51 -0300

    Create SQL views for business intelligence reporting
    This script creates multiple views for the DROP_DATABASE schema, including vw_inscripciones, vw_cursada, vw_finales, vw_pagos, vw_facturacion, and vw_encuestas. These views aggregate data related to course enrollments, evaluations, payments, and surveys for business intelligence purposes.

---
+++ data/script_creación_BI.sql
@@ -0,0 +1,203 @@
+use GD2C2025;
+GO
+
+CREATE VIEW DROP_DATABASE.vw_inscripciones AS
+SELECT
+    YEAR(i.fechaInscripcion) AS anio,
+    MONTH(i.fechaInscripcion) AS mes,
+    s.id as sedeId,
+    cat.id as categoriaId,
+    cu.turnoId as turnoId,
+    COUNT(*) AS cant_inscripciones,
+    SUM(CASE WHEN i.estado = 'Aprobada' THEN 1 ELSE 0 END) AS cant_aprobadas,
+    SUM(CASE WHEN i.estado = 'Rechazada' THEN 1 ELSE 0 END) AS cant_rechazadas
+FROM DROP_DATABASE.Inscripcion_Curso i
+    JOIN DROP_DATABASE.Curso cu ON cu.codigoCurso = i.codigoCurso
+    JOIN DROP_DATABASE.Sede s ON s.id = cu.sedeId
+    JOIN DROP_DATABASE.Categoria cat ON cat.id = cu.categoriaId
+GROUP BY
+    YEAR(i.fechaInscripcion),
+    MONTH(i.fechaInscripcion),
+    s.id,
+    cat.id,
+    cu.turnoId;
+GO
+
+
+CREATE VIEW DROP_DATABASE.vw_cursada AS
+with alumnos_curso as (
+    SELECT
+        cu.codigoCurso,
+        er.legajoAlumno,
+        MIN(er.nota) AS notaMinima,
+        tp.nota AS notaTP,
+        CASE WHEN MIN(er.nota) >= 4 and tp.nota >= 4 THEN 1 ELSE 0 END AS aprobado,
+        CASE WHEN MIN(er.nota) is NULL or MIN(er.nota) < 4 or tp.nota < 4 THEN 1 ELSE 0 END AS desaprobado
+    FROM DROP_DATABASE.Curso cu 
+    JOIN DROP_DATABASE.Modulo_x_Curso mxc 
+        ON cu.codigoCurso = mxc.cursoId
+    JOIN DROP_DATABASE.Modulo_de_curso_tomado_en_evaluacion mce 
+        ON mxc.moduloId = mce.modulo
+    JOIN DROP_DATABASE.Evaluacion e 
+        ON mce.evaluacionId = e.id
+    JOIN DROP_DATABASE.Evaluacion_Rendida er 
+        ON er.evaluacionId = e.id
+    JOIN DROP_DATABASE.TP_Alumno tp on tp.legajoAlumno=er.legajoAlumno and tp.curso=cu.codigoCurso
+    GROUP BY 
+        cu.codigoCurso,
+        er.legajoAlumno,
+        tp.nota
+    )
+SELECT
+    YEAR(cu.fechaInicio) AS anio,
+    MONTH(cu.fechaInicio) AS mes,
+    cu.sedeId,
+    cu.categoriaId,
+    SUM(ac.aprobado) AS cant_aprobados,
+    SUM(ac.desaprobado) AS cant_desaprobados
+FROM alumnos_curso ac
+JOIN DROP_DATABASE.Curso cu 
+    ON cu.codigoCurso = ac.codigoCurso
+GROUP BY
+    YEAR(cu.fechaInicio),
+    MONTH(cu.fechaInicio),
+    cu.sedeId,
+    cu.categoriaId;
+
+GO
+
+
+CREATE VIEW DROP_DATABASE.vw_finales AS
+SELECT 
+    YEAR(cu.fechaInicio) AS anio,
+    MONTH(cu.fechaInicio) AS mes,
+    cu.sedeId,
+    cu.categoriaId,
+
+    -- Rango etario del alumno
+    CASE 
+        WHEN DATEDIFF(year, a.fechaNacimiento, GETDATE()) < 25 THEN '<25'
+        WHEN DATEDIFF(year, a.fechaNacimiento, GETDATE()) BETWEEN 25 AND 35 THEN '25-35'
+        WHEN DATEDIFF(year, a.fechaNacimiento, GETDATE()) BETWEEN 36 AND 50 THEN '35-50'
+        ELSE '>50'
+    END AS rangoAlumno,
+
+    -- Rango etario del profesor
+    CASE 
+        WHEN DATEDIFF(year, p.fechaNacimiento, GETDATE()) < 25 THEN '<25'
+        WHEN DATEDIFF(year, p.fechaNacimiento, GETDATE()) BETWEEN 25 AND 35 THEN '25-35'
+        WHEN DATEDIFF(year, p.fechaNacimiento, GETDATE()) BETWEEN 36 AND 50 THEN '35-50'
+        ELSE '>50'
+    END AS rangoProfesor,
+
+    SUM(CASE WHEN fr.presente = 1 THEN 1 ELSE 0 END) AS cant_presentes,
+    SUM(CASE WHEN fr.presente = 0 or fr.presente IS NULL THEN 1 ELSE 0 END) AS cant_ausentes,
+    AVG(fr.nota) AS notaPromedio
+
+FROM DROP_DATABASE.Final f
+JOIN DROP_DATABASE.Final_rendido fr 
+    ON f.Final_Nro = fr.finalId
+JOIN DROP_DATABASE.Curso cu 
+    ON cu.codigoCurso = f.curso
+JOIN DROP_DATABASE.Alumno a 
+    ON a.legajoAlumno = fr.legajoAlumno 
+JOIN DROP_DATABASE.Profesor p
+    ON p.id = cu.profesorId   
+
+GROUP BY
+    YEAR(cu.fechaInicio),
+    MONTH(cu.fechaInicio),
+    cu.sedeId,
+    cu.categoriaId,
+    CASE 
+        WHEN DATEDIFF(year, a.fechaNacimiento, GETDATE()) < 25 THEN '<25'
+        WHEN DATEDIFF(year, a.fechaNacimiento, GETDATE()) BETWEEN 25 AND 35 THEN '25-35'
+        WHEN DATEDIFF(year, a.fechaNacimiento, GETDATE()) BETWEEN 36 AND 50 THEN '35-50'
+        ELSE '>50'
+    END,
+    CASE 
+        WHEN DATEDIFF(year, p.fechaNacimiento, GETDATE()) < 25 THEN '<25'
+        WHEN DATEDIFF(year, p.fechaNacimiento, GETDATE()) BETWEEN 25 AND 35 THEN '25-35'
+        WHEN DATEDIFF(year, p.fechaNacimiento, GETDATE()) BETWEEN 36 AND 50 THEN '35-50'
+        ELSE '>50'
+    END;
+GO
+
+
+CREATE VIEW DROP_DATABASE.vw_pagos AS
+SELECT
+    YEAR(p.fecha) AS anio,
+    MONTH(p.fecha) AS mes,
+    p.medioPagoId,          
+    SUM(p.importe) AS montoPagado,
+    SUM(
+        CASE 
+            WHEN p.fecha > f.fechaVencimiento THEN p.importe 
+            ELSE 0 
+        END
+    ) AS montoFueraTermino
+
+FROM DROP_DATABASE.Pago p
+JOIN DROP_DATABASE.Factura f 
+    ON p.facturaNumero = f.facturaNumero
+JOIN DROP_DATABASE.Medio_Pago mp 
+    ON p.medioPagoId = mp.id
+
+GROUP BY
+    YEAR(p.fecha),
+    MONTH(p.fecha),
+    p.medioPagoId;
+GO
+
+
+CREATE VIEW DROP_DATABASE.vw_facturacion AS
+SELECT
+    YEAR(fact.fechaEmision) AS anio,
+    MONTH(fact.fechaEmision) AS mes,
+    cu.sedeId,
+    cu.categoriaId,
+
+    SUM(fact.importeTotal) AS importeTotal,
+
+    SUM(
+        CASE 
+            WHEN p.facturaNumero IS NULL THEN fact.importeTotal
+            ELSE 0
+        END
+    ) AS importeAdeudado
+
+FROM DROP_DATABASE.Factura fact
+JOIN DROP_DATABASE.Detalle_Factura df 
+    ON fact.facturaNumero = df.facturaNumero
+JOIN DROP_DATABASE.Curso cu 
+    ON df.codigoCurso = cu.codigoCurso
+
+LEFT JOIN DROP_DATABASE.Pago p 
+    ON p.facturaNumero = fact.facturaNumero
+
+GROUP BY
+    YEAR(fact.fechaEmision),
+    MONTH(fact.fechaEmision),
+    cu.sedeId,
+    cu.categoriaId;
+
+GO
+
+CREATE VIEW vw_encuestas AS
+SELECT 
+
+
+GO
+vw_encuestas
+
+→ alimenta BI_FACT_ENCUESTAS
+
+Contiene:
+
+cantidad satisfechos
+
+cantidad neutrales
+
+cantidad insatisfechos
+
+por: tiempo, sede, categoría, rango profesor, bloque de satisfacción
